a6bdabec98557bae6848dbcd3617f39ab46f37c9
IT2030DSatnic <S01031109@acad.tri-c.edu>
2018-04-09 22:02:29 -0400

    Initial project files commit

---
+++ Lab08a_FacebookAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab08a_FacebookAuth.Startup))]
+namespace Lab08a_FacebookAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
